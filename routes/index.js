79559aac0c155d2a21012c3971dc69f75bd1b428
unknown <richwbates@gmail.com>
2017-12-20 08:54:09 -0500

    created routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const query = require('../js/queries');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+//add order to reciept
+router.put('/placeorder', (req, res, next) => {
 
 });
+
+//remove order from menu
+router.delete('/removeorder', (req, res, next) => {
+
+});
+
+// process a coupon
+router.put('/coupon', (req, res, next) => {
+
+});
+
+//print check and close out order
+router.put('/printcheck', (req, res, next) => {
+
+});
+
+//get menu list from selected menu section
+router.get('/getmenu/:section', (req, res, next) => getMenu(req.body.section));
 
 module.exports = router;
