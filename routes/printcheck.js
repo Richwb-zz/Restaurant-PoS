6287798f2dda4d267fc5c697926e7316e5456ba6
unknown <richwbates@gmail.com>
2017-12-20 12:34:35 -0500

    route to print and process the check

---
+++ routes/printcheck.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const models = require("../models/index.js");
+
+//print check and close out order
+router.put('/printcheck', (req, res, next) => {
+    
+});
