77744aba84eaf5b4813c236f66b83d8d3051ba2e
unknown <richwbates@gmail.com>
2017-12-20 18:16:55 -0500

    /:section route retrieves all items in that menu section from DB

--- routes/menu.js
+++ routes/menu.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const models = require("../models/index.js");
+const menu = models.menu;
 
 //get menu list from selected menu section
-router.get('/:section', (req, res, next) => res.json(getMenu(req.body.section)));
+router.get('/:section', (req, res, next) => {
+    menu.findAll({
+        where: {
+            meal_type: req.headers.category
+        }
+    })
+    .then(result =>{
+        console.log(result[0].dataValues);
+
+        res.json(result[0].dataValues);
+    })
+    .catch(error => (error));
+});
 
 module.exports = router;
