0c8197b547bcd47fe5fe7e8bb117bebd32fc75e4
unknown <richwbates@gmail.com>
2017-12-20 17:11:31 -0500

    renamed file

--- routes/printcheck.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const models = require("../models/index.js");
-
-//print check and close out order
-router.put('/printcheck', (req, res, next) => {
-    
-});
