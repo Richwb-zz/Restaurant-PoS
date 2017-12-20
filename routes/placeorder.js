9125beebd13be0ef65328523ae69a4523709c5ad
unknown <richwbates@gmail.com>
2017-12-20 12:34:18 -0500

    added place and remove order routes

---
+++ routes/placeorder.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const models = require("../models/index.js");
+
+//add order to reciept
+router.put('/place', (req, res, next) => {
+
+});
+
+//remove order from menu
+router.delete('/remove', (req, res, next) => {
+
+});
