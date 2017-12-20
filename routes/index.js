613f31ee64000c941698b63064bdcf31f59fc0e8
unknown <richwbates@gmail.com>
2017-12-20 12:32:50 -0500

    seperated routes into different files

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const query = require('../js/queries');
-
-//add order to reciept
-router.put('/placeorder', (req, res, next) => {
-
-});
-
-//remove order from menu
-router.delete('/removeorder', (req, res, next) => {
-
-});
-
-// process a coupon
-router.put('/coupon', (req, res, next) => {
-
-});
-
-//print check and close out order
-router.put('/printcheck', (req, res, next) => {
-
-});
 
 //get menu list from selected menu section
-router.get('/getmenu/:section', (req, res, next) => getMenu(req.body.section));
+router.get('/', (req, res, next) => {
+    
+});
 
 module.exports = router;
