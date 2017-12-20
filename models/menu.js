076b4db9bc1e4458eef4e5a0b06ef8c779cf656e
unknown <richwbates@gmail.com>
2017-12-20 08:52:42 -0500

    corrected field declaration errors

--- models/menu.js
+++ models/menu.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
 			allowNull: false
 		},
 		meal_type:{
-			type: DataTypes.String,
+			type: DataTypes.STRING,
 			allowNull: false,
 			validate:{
 				len: {
