3425bc1502cec4396d841252b998893a2bce918b
unknown <richwbates@gmail.com>
2017-12-20 17:02:28 -0500

    renamed file

---
+++ routes/check.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const models = require("../models/index.js");
+
+//print check and close out order
+router.put('/', (req, res, next) => {
+    
+});
+
+module.exports = router;
