3a3b3f8264c573bf6510fc666496c914a909cdc6
Jim Reinknecht <captainjimscharters@gmail.com>
2017-12-29 22:25:36 -0500

    added tables functionality for table page. not tested

--- client/src/Pages/Tables/Tables.js
+++ client/src/Pages/Tables/Tables.js
@@ -1,12 +1,160 @@
 import React, { Component } from 'react'
-import { Button, Grid, Row, Col, Container } from 'react-bootstrap'
+import { Jumbotron, Button, Grid, Row, Col, Container } from 'react-bootstrap'
+import Tables from '../../components/Tables'
 
 class Tables extends Component {
-    state = {}
+    state = {
+        tables: {
+            table1: {
+                name: "Table 1",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table2: {
+                name: "Table 2",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table3: {
+                name: "Table 3",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table4: {
+                name: "Table 4",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table5: {
+                name: "Table 5",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table6: {
+                name: "Table 6",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table7: {
+                name: "Table 7",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table8: {
+                name: "Table 8",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table9: {
+                name: "Table 9",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table10: {
+                name: "Table 10",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            }, table11: {
+                name: "Table 11",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            },
+            table12: {
+                name: "Table 12",
+                isOccupied: false,
+                guestNumber: null,
+                server: {},
+                bill: {
+                    id: null,
+                    items: {},
+                    total: null
+                }
+            }
+        }
+    }
+    tableClickHandler = (event) => {
 
+    }
     render() {
         return (
-            <h1> tables </h1>
+            <Container>
+                <Jumbotron>
+                    <h1> Eat At Joes </h1>
+                </Jumbotron>
+                <Row>
+                    {this.state.tables.map((tables,index) => {
+                        return (
+                    <Table table={table} onClick={(event) => this.tableClickHandler(event)} key={index}/>
+                    )
+                    })}
+                </Row>
+            </Container>
         )
     }
 
