f6c355a98f8ec9d10f96f89e7b6141c8521bb667
unknown <richwbates@gmail.com>
2018-01-03 17:41:43 -0500

    converted to older app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,162 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Grid, Row, Col } from 'react-bootstrap'
-import Navbar from './components/Navbar'
-import Order from './Pages/Order'
-import Tables from './Pages/Tables'
-import Table from './Pages/Tables/Table'
+import { Button, Grid, Row, Col, Nav } from 'react-bootstrap'
+import API from './utils/API'
+import Order from './components/Order'
 
-const App = () => {
-    <Router>
-        <div>
-            <Navbar />
-            <Switch>
-                <Route exact path="/" component={Tables} />
-                <Route exact path="/table/:id" component={Table} />
-                <Route exact path="/order/:id" component={Order} />
-                <Route component={NoMatch} />
-            </Switch>
-        </div>
-    </Router>;
+class App extends Component {
 
-
+  state = {
+    tables: {
+      table1: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table2: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table3: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table4: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table5: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table6: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table7: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table8: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table9: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table10: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      }, table11: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      },
+      table12: {
+        isOccupied: false,
+        guestNumber: null,
+        bill: {
+          id: null,
+          items: {},
+          total: null
+        }
+      }
+    },
+    servers: {},
+    menu: {},
+    activePage: ""
+  }
+  getMenu = () => {
+    API.getMenu().then( results => {
+      let newMenu = results.data
+      this.setState({menu: newMenu}, ()=>{
+        console.log(this.state.menu)
+      })
+    }).catch( error => {
+      if (error) console.log(error)
+    })
+  }
+  getServers= ()=>{
+    API.getServers().then(results => {
+      let newServers = results.data
+      this.setState({servers: newServers }, () => {
+        console.log(this.state.servers)
+      })
+    }).catch(error => {
+      if (error) console.log(error)
+    })
+  }
+  getTables = ()=>{
+    API.getTables().then(results => {
+      let newTables = results.data
+      //not finished, need to itierate through data
+      //this.setState({ servers: newServers }, () => {
+       // console.log(this.state.servers)
+      //})
+    }).catch(error => {
+      if (error) console.log(error)
+    })
+  }
+  render() {
+    return (
+      <Order />
+    );
+  }
 }
+
 export default App;
