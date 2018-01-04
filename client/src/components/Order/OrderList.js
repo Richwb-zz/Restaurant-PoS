c9674136a7ab465411671f344a35f6f7bf8ddc8a
unknown <richwbates@gmail.com>
2018-01-04 15:17:06 -0500

    added function to render order list in a table and incuded a delete button

--- client/src/components/Order/OrderList.js
+++ client/src/components/Order/OrderList.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import { Button,Panel, Grid, Row, Col, Container } from 'react-bootstrap';
+
+const getOrderItems = (newOrders) => {
+    return (
+        <Row>
+        {newOrders.map((item, index) => {
+            return (
+                <div class={"row" + index}>
+                    <Col md={6}>
+                        {item}
+                    </Col>
+                    <Col md={6} >
+                        <Button id={"row" + index} onClick={(event) => removeItem(event)}>X</Button>
+                    </Col>
+                </div>
+            );
+        
+        })}
+        </Row>
+    );
+};
+
+const removeItem = (event) => {
+    const element = document.getElementsByClassName(event.target.id)
+    element[0].parentNode.removeChild(element[0]);
+
+}
 
 const OrderList = props => {
     return(
-        props.newOrders
+        getOrderItems(props.newOrders)                 
     );
 };
 
