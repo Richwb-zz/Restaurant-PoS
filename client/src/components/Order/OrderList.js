3450baf69fd0105a663729002dcfded112d6421f
unknown <richwbates@gmail.com>
2018-01-05 19:57:23 -0500

    changed into a class to process removing of items from state

--- client/src/components/Order/OrderList.js
+++ client/src/components/Order/OrderList.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
 import { Button,Panel, Grid, Row, Col, Container } from 'react-bootstrap';
 
-const getOrderItems = (newOrders) => {
-    return (
-        <Row>
-        {newOrders.map((item, index) => {
-            return (
-                <div class={"row" + index}>
-                    <Col md={6}>
-                        {item}
-                    </Col>
-                    <Col md={6} >
-                        <Button id={"row" + index} onClick={(event) => removeItem(event)}>X</Button>
-                    </Col>
-                </div>
-            );
-        
-        })}
-        </Row>
-    );
-};
+class OrderList extends Component {
+    getOrderItems = (props) => {
+        return (
+            <Row>
+            {props.newOrderList.map((item, index) => {
+                return (
+                    <div>
+                        <Col md={4}>
+                            {item.name}
+                        </Col>
+                        <Col id={item.name + "count"} md={4}>
+                            {item.quantity}
+                        </Col>
+                        <Col md={4} >
+                            <Button id={item.name + "delete"} onClick={(event) => this.getItemToRemove(event)}>X</Button>
+                        </Col>
+                    </div>
+                );
+            
+            })}
+            </Row>
+        );
+    };
 
-const removeItem = (event) => {
-    const element = document.getElementsByClassName(event.target.id)
-    element[0].parentNode.removeChild(element[0]);
+    getItemToRemove = (event) => {
+        const itemToRemove = event.target.id.replace("delete","");
+        this.props.removeFromOrder(itemToRemove);
+    }
 
+     render(){
+        return(
+            this.getOrderItems(this.props)
+        )
+    };
 }
-
-const OrderList = props => {
-    return(
-        getOrderItems(props.newOrders)                 
-    );
-};
 
 export default OrderList;
