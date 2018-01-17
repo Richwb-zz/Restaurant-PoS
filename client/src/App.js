8521a7734f17fe60ae828d769543ddc21aa508c0
unknown <richwbates@gmail.com>
2018-01-17 11:17:58 -0500

    created conditional for user login and an user clear for logging out

--- client/src/App.js
+++ client/src/App.js
@@ -286,8 +286,16 @@ class App extends Component {
   }
 
   setUser = (name) => {
+    if(typeof name === "string"){
+        this.setState({
+        user: name
+      })
+    }
+  }
+
+  unsetUser = () => {
     this.setState({
-      user: name
+      user: null
     })
   }
 
@@ -444,7 +452,7 @@ class App extends Component {
     return (
       <Hoc>
         <Grid fluid>
-          <Navbar activePage={this.state.activePage} handleSelect={this.activePageHandler} activeTable={this.state.activeTable} />
+          <Navbar activePage={this.state.activePage} handleSelect={this.activePageHandler} activeTable={this.state.activeTable} loggedInUser={this.state.user} logOut={this.unsetUser}/>
         </Grid>
         <Grid>
           <Row>
