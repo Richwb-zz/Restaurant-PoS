e90646b56bdb1fd7ef7898a6d93be31be90f58c3
Jim Reinknecht <captainjimscharters@gmail.com>
2018-01-17 16:23:02 -0500

    added react-alerts to package, needs implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+import App from './App'
 import registerServiceWorker from './registerServiceWorker';
 
+const options = {
+    timeout: 5000
+}
+class Root extends Component {
+    render() {
+        return (
+            <AlertProvider template={AlertTemplate} {...options}>
+                <App />
+            </AlertProvider>
+        )
+    }
+}
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+render(<Root />, document.getElementById('root'))
 registerServiceWorker();
