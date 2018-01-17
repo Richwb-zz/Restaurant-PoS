2003c6e3983dfa46e0ff805c403e53368f7f5dc1
unknown <richwbates@gmail.com>
2018-01-16 21:51:38 -0500

    added /login/:code get route

--- routes/servers.js
+++ routes/servers.js
@@ -16,4 +16,16 @@ router.post('/', (req, res, next) => {
     // ADD SERVER INFO HERE
     res.send('Request received, Server Not added, code not there')
 });
+
+router.get('/login/:code', (req,res,next) => {
+    servers.findOne({}).where("code").equals(req.params.code)
+        .then(result => {
+            console.log("rep " + result.name)
+                res.json(result)
+        })
+        .catch(error =>{
+            res.json(error);
+        })
+})
+
 module.exports = router;
