a74ef0c07c5826f4c04bec6a678477c40193a022
Jim Reinknecht <captainjimscharters@gmail.com>
2018-01-16 21:05:58 -0500

    heroku testing

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname,"client/build")));
 app.use(cors());
 
 app.use('/', index);
