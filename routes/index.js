039e9fa62e74c9b4d3880c996be00f44a69646c5
Jim Reinknecht <captainjimscharters@gmail.com>
2018-01-16 20:37:41 -0500

    WW

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express   = require('express');
 const router    = express.Router();
+const mongoose 	= require('mongoose');
+const models 	= require('../models/all-models.js');
 
-
-
+//get menu list from selected menu section
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
