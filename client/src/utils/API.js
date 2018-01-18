50a09ce963126e1fcf69762ae1c96e5f7303fc93
Jim Reinknecht <captainjimscharters@gmail.com>
2018-01-18 09:08:19 -0500

    heroku build #2

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,7 +10,7 @@ export default {
         return axios.get("/servers")
     },
     getTables: ()=>{
-        return axios.get("/unpaid")
+        return axios.get("/checks/unpaid")
     },
     seatGuests: (seating)=>{
         //seats new guests
